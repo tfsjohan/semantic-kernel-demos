5ea6949ac6f42973fdb5ccb1c09f0b98a4b48850
Johan Nordberg <johan@ifokus.se>
2024-09-26 15:11:35 +0200

    Basic chat demo with system prompt

---
+++ SemanticKernelDemos.BasicChat/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var systemPrompt = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "Prompt.md"));
+
+var builder = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(
+        "gpt-4o-mini",
+        config["AOAI:Endpoint"]!,
+        config["AOAI:ApiKey"]!);
+
+var kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var history = new ChatHistory(systemPrompt);
+
+while (true)
+{
+    // Get user input
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.Write("User > ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    history.AddUserMessage(Console.ReadLine()!);
+
+    OpenAIPromptExecutionSettings openAiPromptExecutionSettings = new()
+    {
+        MaxTokens = 200,
+        Temperature = 0.9,
+    };
+
+    // Get the response from the AI
+    var response = chatCompletionService.GetStreamingChatMessageContentsAsync(
+        history,
+        executionSettings: openAiPromptExecutionSettings,
+        kernel: kernel);
+
+
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write("\nAssistant > ");
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    var combinedResponse = string.Empty;
+    await foreach (var message in response)
+    {
+        //Write the response to the console
+        Console.Write(message);
+        combinedResponse += message;
+    }
+
+    Console.WriteLine();
+
+    // Add the message from the agent to the chat history
+    history.AddAssistantMessage(combinedResponse);
+}
---
+++ SemanticKernelDemos.BasicChat/Prompt.md
@@ -0,0 +1,3 @@
+You are a and old stubborn developer that refuses to learn new and modern tech. You will answer questions about
+programming and software development with a very old and outdated perspective. You prefer obscure and outdated
+technologies and you are very proud of it. You are very stubborn and you will refuse to learn anything new.
